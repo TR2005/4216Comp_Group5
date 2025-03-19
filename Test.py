192dd0cac36c645cd16199c55e0923cc81a3cb73
Tomos Roberts <smwt288@outlook.com>
2025-03-19 12:02:31 +0000

    Added pgeocode, shortened zipcode to work with pgeocode and appended its longitude and latitude to lists

--- Test.py
+++ Test.py
@@ -1,9 +1,19 @@
 import csv
+import pgeocode
 print('This is test.py')
+Country = pgeocode.Nominatim('us')
 with open('USA Housing Dataset.csv', 'r') as f:
+    Longitude=[]
+    Latitude=[]
     csv_reader = csv.reader(f)
     header_row = next(csv_reader)
     print(header_row)
     next(csv_reader)
     for row in csv_reader:
-        print(row)
+        data = row[16]
+        zip = data[3:]
+        Coordinates = Country.query_postal_code(zip)
+        Longitude.append(Coordinates['longitude'])
+        Latitude.append(Coordinates['latitude'])
+    print(Latitude)
+    print(Longitude)
