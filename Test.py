07554fcb09c9aeb6b7d4fb8edc7193f4111b24f4
Reece Woodward <R.Woodward@2024.ljmu.ac.uk>
2025-03-19 12:19:50 +0000

    testing matplotlib for my questions and their associated collums

--- Test.py
+++ Test.py
@@ -1,9 +1,37 @@
 import csv
+import matplotlib.pyplot as plt
+
 print('This is test.py')
 with open('USA Housing Dataset.csv', 'r') as f:
     csv_reader = csv.reader(f)
     header_row = next(csv_reader)
-    print(header_row)
-    next(csv_reader)
+    
+    #attatching row 1 to a list so it can be used by matplotlib
+
+    
+
+    YearBuilt = []
+    Price = []
     for row in csv_reader:
-        print(row)
+        data = row[12]
+        data1 = row[1]
+        YearBuilt.append(data)
+        Price.append(data1)
+
+    YearBuilt.sort()
+    Price.sort
+    
+
+    fig, ax = plt.subplots()
+
+    ax.plot(YearBuilt, Price)
+
+    plt.show()
+
+    
+
+   
+
+
+
+
