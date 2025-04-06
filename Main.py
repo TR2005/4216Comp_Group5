195b4883914a301e8392701bea113c6994c91f36
oliver9362 <oliverleng04@gmail.com>
2025-04-06 20:50:06 +0100

    Changed housing_prices to data to be used across all visualisations. Added My first visualisation

--- Main.py
+++ Main.py
@@ -1,6 +1,11 @@
 import csv
 import matplotlib.pyplot as plt 
 import pandas as pd 
+import matplotlib.patches as mpatches
+
+#Read dataset and store it in variable "data"
+data = pd.read_csv("USA Housing Dataset.csv")
+
 def option_print_dataset():
     print("You selected Print Dataset.")
     #runs through the dataset and prints it
@@ -39,9 +44,8 @@ def option_tomos():
     # place your visualisation code here please. @TR2005
 def option_reece():
     print("you selected user: Reece")
-    housing_prices = pd.read_csv("USA Housing Dataset.csv")
     #only getting the rows which have renovations 
-    unsorted_renovated_true = housing_prices[housing_prices["yr_renovated"]>0]
+    unsorted_renovated_true = data[data["yr_renovated"]>0]
     #sorting and removing dupes on the rows needed for the visualisation
     renovated_true_sorted = unsorted_renovated_true.sort_values(by="yr_renovated")
     renovated_true_nodupe = renovated_true_sorted.drop_duplicates("yr_renovated")
@@ -58,10 +62,53 @@ def option_reece():
     ax.grid(True)
     plt.show()
 
+#Set bar colour conditions for city prices visualisation
+def bar_colour(price):
+    if price <300000:
+        return "green"
+    elif price >=300000 and price < 800000:
+        return "orange"
+    else:
+        return "red"
 
 def option_oliver():
     print("you selected user: Oliver")
-    # place your visualisation code here please. @oliver9362
+    
+    #Group dataset by city
+    data_grouped = data.groupby("city")["price"].mean()
+
+    #Assign bars colours based on price
+    colours = [bar_colour(p) for p in data_grouped]
+
+    #Create the plot
+    fig, ax = plt.subplots(figsize = (12, 6))
+    ax.bar(data_grouped.index, data_grouped, color = colours)
+       
+    green_patch = mpatches.Patch(color="green", label="Affordable Place To Live (average under $300000)")
+    orange_patch = mpatches.Patch(color="orange", label="Average Cost Place To Live(average between $300000 and $800000")
+    red_patch = mpatches.Patch(color="red", label="Expensive Place To Live(Over $800000)")
+
+    ax.legend(handles=[green_patch, orange_patch,red_patch])
+
+    #Labels and title
+    ax.set_xlabel("City")
+    ax.set_ylabel("Average Price ($)")
+    ax.set_title("House Price Comparison Between Cities")
+
+    #Rotate labels and add lines on y axis
+    ax.set_xticklabels(data_grouped.index, rotation=90, fontsize = 9)
+    ax.yaxis.grid(True)
+
+    #Stop the graph using scientific notation
+    plt.ticklabel_format(style = 'plain', axis = "y")
+
+    #Make sure all labels are visible in the graph
+    plt.tight_layout()
+
+    #Show the graph
+    plt.show()
+
+
 def option_sam():
     print("you selected user: Sam")
     #place your visualisations code here sam
